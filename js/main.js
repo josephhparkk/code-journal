b36a11c58eaa788743feff296ca226baac7ed1f4
Joseph Park <parkjoseph54@gmail.com>
2022-11-03 01:44:50 +0000

    completed user can view their entrie

--- js/main.js
+++ js/main.js
@@ -20,35 +20,18 @@ newForm.addEventListener('submit', function (event) {
 
   data.nextEntryId++;
   data.entries.unshift(newFormData);
-
   newForm.reset();
   resetPhoto.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  clickGoToEntryPage(event);
+  unorderedList.prepend(renderEntry(newFormData));
+
 });
+var unorderedList = document.querySelector('ul');
 
-/* ENTRIES:
-Click 'SAVE' button on NEW ENTRY page --> lead to entries page
-click 'NEW' --> go to new entry page
-
-when an entry is SAVED --> show up in entries and need to show for every single one
-newest one is saved on top
-
-*/
-
-var formContainerPage = document.querySelector('.form-container');
-var entryContainerPage = document.querySelector('.entry-container');
-
-function setEntriesPage(entry) {
-  var div1 = document.createElement('div');
-  div1.setAttribute('class', 'container entry-container');
-  div1.setAttribute('data-view', 'entries');
-
-  var ul = document.createElement('ul');
-  ul.setAttribute('class', 'row');
-  div1.appendChild(ul);
-
+function renderEntry(entry) {
   var li = document.createElement('li');
   li.setAttribute('class', 'column-full entries');
-  ul.appendChild(li);
 
   var div2 = document.createElement('div');
   div2.setAttribute('class', 'row');
@@ -64,8 +47,8 @@ function setEntriesPage(entry) {
   div3.appendChild(img);
 
   var div4 = document.createElement('div');
-  div4.setAttribute('class', 'column-half');
-  div3.appendChild(div4);
+  div4.setAttribute('class', 'column-half description');
+  div2.appendChild(div4);
 
   var head2 = document.createElement('h2');
   var paragraph = document.createElement('p');
@@ -74,24 +57,38 @@ function setEntriesPage(entry) {
   div4.appendChild(head2);
   div4.appendChild(paragraph);
 
-  return div1;
+  return li;
 }
+var noEntryMessage = document.querySelector('.no-message');
+var formContainerPage = document.querySelector('.form-container');
+var entryContainerPage = document.querySelector('.entry-container');
 
-window.addEventListener('DOMContentLoaded', function (entry) {
-  if (data.view === 'entry-form') {
-    entryFormView.className = 'entries';
-    // entryFormView.classList.remove('entry-form');
-    formContainerPage.className = 'hidden';
-    entriesView.classList.remove('hidden');
+window.addEventListener('DOMContentLoaded', function (event) {
+  if (data.view === 'entries') {
+    clickGoToEntryPage(event);
+  } else if (data.view === 'entry-form') {
+    clickNewForNewEntry(event);
   }
   for (var i = 0; i < data.entries.length; i++) {
-    entryContainerPage.append(setEntriesPage(data.entries[i]));
+    entryContainerPage.append(renderEntry(data.entries[i]));
   }
+});
+
+var entriesTab = document.querySelector('.entries-tab');
+entriesTab.addEventListener('click', clickGoToEntryPage);
+
+function clickGoToEntryPage(event) {
+  formContainerPage.classList.add('hidden');
+  entryContainerPage.classList.remove('hidden');
+  noEntryMessage.classList.add('hidden');
+  data.view = 'entries';
 }
-);
 
-// var saveButton = document.querySelector('button');
-// var formContainerPage = document.querySelector('.form-container');
+var newButton = document.querySelector('.new-button');
+newButton.addEventListener('click', clickNewForNewEntry);
 
-var entryFormView = document.querySelector('entry-form');
-var entriesView = document.querySelector('entries');
+function clickNewForNewEntry(event) {
+  entryContainerPage.classList.add('hidden');
+  formContainerPage.classList.remove('hidden');
+  data.view = 'entry-form';
+}
